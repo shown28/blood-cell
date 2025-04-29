28b7b35b2f847d943370194a817529379c49139c
Shown <manuelshown@gmail.com>
2025-04-29 18:48:10 +0530

    after server hosted

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,3 +1,6 @@
-const serverUrl = `http://localhost:3001`
+const serverUrl = "https://bloodcell-server-l4r8.onrender.com"
+//  `http://localhost:3001`
+//"https://bloodcell-server-l4r8.onrender.com"
+// 
 
 export default serverUrl
