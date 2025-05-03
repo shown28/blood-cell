2661d776b366ce6e4b81d0cfbac2d649dd30d766
Shown <manuelshown@gmail.com>
2025-05-03 16:07:02 +0530

    email issue solved

--- src/pages/VoluntaryDonors.jsx
+++ src/pages/VoluntaryDonors.jsx
@@ -85,7 +85,7 @@ const VoluntaryDonors = () => {
                                 </div>
                                 <div class="flex justify-center p-6 pt-2 gap-7">
                                     <button onClick={()=>requestMessage(data.email)} class="min-w-32  rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
-                                        Message
+                                        Request Details
                                     </button>
                                 </div>
                             </div>
--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,6 +1,7 @@
-const serverUrl = "https://bloodcell-server-l4r8.onrender.com"
+const serverUrl =`https://new-bloodcell-server.onrender.com` 
 //  `http://localhost:3001`
 //"https://bloodcell-server-l4r8.onrender.com"
 // 
+
 
 export default serverUrl
